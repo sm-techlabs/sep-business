893652b786bc255b5bc41fc8d8248ea15669d402
Sam Mosios <sam.mosios@gmail.com>
2025-10-04 00:30:50 +0200

    chore: Updates frontend

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -14,3 +14,14 @@ describe('GET /api/hello', () => {
     expect(typeof res.body.time).toBe('string');
   });
 });
+
+
+describe('GET /api/shouldFail', () => {
+  it('should return a hello message with a timestamp', async () => {
+    const res = await request(app).get('/api/hello');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('message', 'Hello from the backend!');
+    expect(res.body).toHaveProperty('time');
+    expect(typeof res.body.time).toBe('string');
+  });
+});
