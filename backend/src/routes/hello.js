5d67168f1be5c4d55d3cc3131e468367c6f27d30
Sam Mosios <sam.mosios@gmail.com>
2025-10-04 13:13:19 +0200

    ci: Fixes nesting issue

--- backend/src/routes/hello.js
+++ backend/src/routes/hello.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: "Hello from the backend!", time: new Date().toISOString() });
+  res.json({ message: "The backend greets you!", time: new Date().toISOString() });
 });
 
 export default router;
