dea7823b6bf805487e57595ede0e881955ee25e3
Sam Mosios <sam.mosios@gmail.com>
2025-10-09 20:23:12 +0200

    Adds authentication service

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import health from './routes/health.js';
 import { sequelize, initSampleData } from './models/index.js';
+import authentication from './routes/authentication.js';
+// import authorization from './routes/authorization.js';
 
 const app = express();
 const port = 3000;
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/health', health);
+app.use('/api/authentication', authentication);
+// app.use('/api/authorization', authorization);
 
 const server = app.listen(port, async () => {
   try {
